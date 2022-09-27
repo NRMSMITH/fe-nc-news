7035f4112ae43fade42d730b7c063ffb64568395
NRMSMITH <niamhrmsmith94@hotmail.co.uk>
2022-09-27 11:35:04 +0100

    added isloading and setisloading

--- src/Pages/SingleArticle.jsx
+++ src/Pages/SingleArticle.jsx
@@ -5,14 +5,19 @@ import { getSingleArticle } from "../utils/api";
 export const SingleArticle = () => {
 
     const [singleArticle, setSingleArticle] = useState([]);
+    const [isLoading, setIsLoading] = useState(false)
     const { article_id } = useParams();
 
     useEffect(() => {
+        setIsLoading(true);
         getSingleArticle(article_id)
         .then((data) => {
             setSingleArticle(data.result)
+            setIsLoading(false)
         })
     }, [article_id])
+
+    if(isLoading) return <h4>Getting your article ...</h4>
 
 return (
     <section>
--- src/Pages/Topics.jsx
+++ src/Pages/Topics.jsx
@@ -5,15 +5,18 @@ import { getArticlesByTopic } from '../utils/api'
 
  export const Topics = () => {
     const { topic_name } = useParams();
-
-
+    const [isLoading, setIsLoading] = useState(false)
     const [articles, setArticles] = useState([])
 
 useEffect(() => {
+  setIsLoading(true)
   getArticlesByTopic(topic_name).then((data) => {
     setArticles(data.articles);
+    setIsLoading(false)
   });
 }, [topic_name]);
+
+if (isLoading) return <h4>Getting your articles ...</h4>
 
     return (
       <section>
