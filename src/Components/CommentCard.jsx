23bcd885d1d0f2030c05513ff5e210d19b8265d0
NRMSMITH <niamhrmsmith94@hotmail.co.uk>
2022-09-27 15:22:44 +0100

    added brackets to denote separation of author and votes

--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -1,7 +1,7 @@
 export const CommentCard = ({comment}) => {
     return (
       <li className="comment_card" key={comment.comment_id}>
-          <h4>{comment.author} {comment.votes}</h4>
+          <h4>{comment.author} [{comment.votes}]</h4>
           <p>{comment.body}</p>
       </li>
     );
